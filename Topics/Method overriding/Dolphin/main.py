7b0d10286939badce5f3873656f11ce992c232ec
Alexis Carrère-Moreno <alcamoru@icloud.com>
2022-06-13 07:57:58 +0200

    Initial commit

---
+++ Topics/Method overriding/Dolphin/main.py
@@ -0,0 +1,16 @@
+class Mammal:
+    def __init__(self):
+        self.bio_class = "mammal"
+
+    def greet(self):
+        print("I am a {}!".format(self.bio_class))
+
+
+# create class Dolphin here
+class Dolphin(Mammal):
+    def __init__(self):
+        super().__init__()
+
+    def greet(self):
+        super(Dolphin, self).greet()
+        print("I am a dolphin!")
